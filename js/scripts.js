ae9fdfd382e23ad3bba0ba189fb2b139444637eb
Meguru Yasuda <yasudamel@gmail.com>
2018-08-13 20:11:29 -0700

    fix the li that doubles each time adding new input by adding emply()

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,16 @@ var items = [];
 $(document).ready(function() {
   $("#grocery").submit(function(event) {
     event.preventDefault();
+    $("#foodList").empty();
     var food = $("#food").val();
     items.push(food);
     var itemsCapped = items.map(function(item){
       return item.toUpperCase();
-    // var itemsOrganized = itemsCapped.map(function(donut){
-    //   return donut.sort();
     });
     var itemsOrganized = itemsCapped.sort();
     itemsOrganized.forEach(function(flavor) {
     $("#foodList").append("<li>" + flavor + "</li>");
+
     });
   });
 });
